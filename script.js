5b77fa5e92b8b7c16ab02734d2973f9fcaa2abb5
Dikshit Raj Sharma <214721236+Dikshit-Raj-Sharma@users.noreply.github.com>
2025-06-23 17:52:55 +0000

    Add playGame function to run 5 rounds and declare final result

--- script.js
+++ script.js
@@ -1,13 +1,50 @@
-console.log("Welcome To My Rock Paper Scissor Game")
+console.log("Welcome To My Rock Paper Scissors Game")
 
-let userScore=0;
-let computerScore=0;
-function getComputerChoice(){
-    return Math.floor(Math.random()*3);
+let userScore = 0;
+let computerScore = 0;
+function getComputerChoice() {
+    const choice = Math.floor(Math.random() * 3);
+    if (choice == 0) return "rock";
+    else if (choice == 1) return "paper";
+    else return "scissors";
 }
-console.log(getComputerChoice())
 
-function getUserChoice(){
-    return prompt("Enter your choice: rock, paper or scissor");
+function getUserChoice() {
+    return prompt("Enter your choice: rock, paper or scissors");
 }
-console.log(getUserChoice())
+
+function playRound() {
+    const userChoice = getUserChoice().toLowerCase();
+    const computerChoice = getComputerChoice();
+    if (userChoice == computerChoice) {
+        console.log("Round end in a draw");
+    }
+    else if (userChoice == "rock" && computerChoice == "paper") {
+        console.log("You Lost in this round.")
+        computerScore++;
+    }
+    else if (userChoice == "paper" && computerChoice == "scissors") {
+        console.log("You Lost in this round.")
+        computerScore++;
+    }
+    else if (userChoice == "scissors" && computerChoice == "rock") {
+        console.log("You Lost in this round.")
+        computerScore++;
+    }
+    else {
+        console.log("You Won in this round!")
+        userScore++;
+    }
+    console.log(`Your Score : ${userScore} and Computer Score : ${computerScore}`)
+}
+
+function playGame() {
+    userScore=0;
+    computerScore=0;
+    for (let i = 0; i < 5; i++) {
+        playRound();
+    }
+    if (userScore > computerScore) console.log("You Won the game!");
+    else if (userScore < computerScore) console.log("You Lost the game");
+    else console.log("Game ends in a Draw");
+}
