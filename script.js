73cbc0abc37207e6e46052eedb0f6e5bfa17c4e6
Dikshit Raj Sharma <214721236+Dikshit-Raj-Sharma@users.noreply.github.com>
2025-06-23 17:18:45 +0000

    Add user/computer input functions and score variables

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Is it Linked?")
+console.log("Welcome To My Rock Paper Scissor Game")
+
+let userScore=0;
+let computerScore=0;
+function getComputerChoice(){
+    return Math.floor(Math.random()*3);
+}
+console.log(getComputerChoice())
+
+function getUserChoice(){
+    return prompt("Enter your choice: rock, paper or scissor");
+}
+console.log(getUserChoice())
